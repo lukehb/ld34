1d308edb9ae2f28e8aa7a132b2d26ed815f1047d
luke bermingham <luke.bermingham@my.jcu.edu.au>
2015-12-14 15:03:52 +1000

    Player moves mostly in.

---
+++ Assets/Scripts/KeyListener.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public abstract class KeyListener : MonoBehaviour
+{
+
+    private readonly List<string> keys;
+    private readonly Dictionary<string, float> keysHeld;
+
+    public KeyListener()
+    {
+        this.keys = new List<string>();
+        this.keysHeld = new Dictionary<string, float>();
+    }
+
+    protected void ListenForKey(string key)
+    {
+        this.keys.Add(key);
+    }
+
+    protected abstract void Start();
+
+    protected virtual void Update()
+    {
+        ProcessKeys();
+    }
+
+    public void ProcessKeys()
+    {
+        foreach (string key in keys)
+        {
+            //check if it is held
+            if (Input.GetKey(key))
+            {
+                float holdTime;
+                keysHeld.TryGetValue(key, out holdTime);
+                keysHeld[key] = holdTime + Time.deltaTime;
+            }
+            //it is not held
+            else
+            {
+                if (keysHeld.ContainsKey(key))
+                {
+                    float holdTime = keysHeld[key];
+                    keysHeld.Remove(key);
+                    KeyPressed(key, holdTime);
+                }
+            }
+        }
+    }
+
+    public float KeyHeldFor(string key)
+    {
+        float timeHeld;
+        keysHeld.TryGetValue(key, out timeHeld);
+        return timeHeld;
+    }
+
+    protected abstract void KeyPressed(string key, float holdTime);
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,103 +1,214 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : KeyListener
 {
     //player states
     private enum Moves
     {
         Idle = 0,
         MovingForward = 1,
-        MovingBackward = 2
+        MovingBackward = 2,
+        Jabbing = 3,
+        OneTwo = 4,
+        Uppercut = 5,
+        Dodge = 6,
     }
 
-    private string Anim_WalkingForward = "WalkingForward";
-    private string Anim_WalkingBackward = "WalkingBackward";
-
-    private const float HoldingKeyTime = 0.5f;
+    //parameters to pass to animator
+    private static readonly string Anim_Bool_Walking_Forward = "WalkingForward";
+    private static readonly string Anim_Bool_Walking_Backward = "WalkingBackward";
+    private static readonly string Anim_Trigger_Jabbing = "Jabbing";
+    private static readonly string Anim_Trigger_OneTwoing = "OneTwoing";
+    private static readonly string Anim_Trigger_Dodging = "Dodging";
+    private static readonly string Anim_Trigger_Uppercut = "Uppercut";
+    //names of animations, used to query the animation for state
+    private static readonly string Anim_Idle = "idle";
+    private static readonly string Anim_Jab = "jab";
+    private static readonly string Anim_Walk_Backward = "walkbackward";
+    private static readonly string Anim_Walk_Forward = "walkforward";
+    private static readonly string Anim_One_Two = "onetwo";
+    private static readonly string Anim_Dodge = "dodge";
+    private static readonly string Anim_Uppercut = "uppercut";
+    //CONSTANTS
+    /// <summary>
+    /// The time that must be exceeded to trigger a long press (or hold).
+    /// </summary>
+    private const float KeyLongPressTime = 0.137f;
+    /// <summary>
+    /// To do a double tap on a key both presses must occur within this time.
+    /// </summary>
+    private const float KeyDoubleTapTime = 0.3f;
 
     [SerializeField]
-    private String _attackKey;
+    private string _attackKey;
     [SerializeField]
-    private String _dodgeKey;
+    private string _dodgeKey;
 
     private Animator _anim;
-    private float _timeholdingAttackKey;
-    private float _timeholdingDodgeKey;
-    private Moves _currentMove = Moves.Idle;
+    private int doubleTapCount = 0;
+    private float doubleTapStarted;
 
-	void Start ()
+	protected override void Start ()
 	{
 	    _anim = GetComponent<Animator>();
+        ListenForKey(_attackKey);
+        ListenForKey(_dodgeKey);
 	}
 	
-	void Update () {
-	    ScanInputs();
+	protected override void Update () {
+        base.Update();
         HandleInputs();
 	}
+
+    protected override void KeyPressed(string key, float holdTime)
+    {
+        //attacks
+        if (key.Equals(_attackKey))
+        {
+            if (holdTime < KeyLongPressTime)
+            {
+                bool doJab = true;
+                if (holdTime < KeyDoubleTapTime)
+                {
+                    //uppercut
+                    if (IsCurrentMove(Moves.Dodge))
+                    {
+                        doJab = false;
+                        DoMove(Moves.Uppercut);
+                    }
+                    else
+                    {
+                        //one two
+                        if (DidDoubleTap())
+                        {
+                            DoMove(Moves.OneTwo);
+                            doJab = false;
+                        }
+                    }
+                }
+                //jab
+                if (doJab)
+                {
+                    DoMove(Moves.Jabbing);
+                }
+            }
+            else
+            {
+                DoMove(Moves.Idle);
+            }
+        }
+        //dodges
+        else if (key.Equals(_dodgeKey))
+        {
+            if (holdTime < KeyLongPressTime)
+            {
+                DoMove(Moves.Dodge);
+            }
+            else
+            {
+                DoMove(Moves.Idle);
+            }
+        }
+    }
+
+    bool DidDoubleTap()
+    {
+        if (doubleTapCount == 0)
+        {
+            doubleTapStarted = Time.timeSinceLevelLoad;
+        }
+        doubleTapCount++;
+        if (doubleTapCount >= 2)
+        {
+            if (Time.timeSinceLevelLoad - doubleTapStarted < KeyDoubleTapTime)
+            {
+                doubleTapStarted = 0;
+                doubleTapCount = 0;
+                return true;
+            }
+            doubleTapStarted = 0;
+            doubleTapCount = 0;
+        }
+        return false;
+    }
 
     void DoMove(Moves move)
     {
         switch (move)
         {
             case Moves.MovingForward:
-                _anim.SetBool(Anim_WalkingForward, true);
+                _anim.SetBool(Anim_Bool_Walking_Forward, true);
                 break;
             case Moves.MovingBackward:
-                _anim.SetBool(Anim_WalkingBackward, true);
+                _anim.SetBool(Anim_Bool_Walking_Backward, true);
+                break;
+            case Moves.Jabbing:
+                _anim.SetTrigger(Anim_Trigger_Jabbing);
+                break;
+            case Moves.OneTwo:
+                _anim.SetTrigger(Anim_Trigger_OneTwoing);
+                break;
+            case Moves.Dodge:
+                _anim.SetTrigger(Anim_Trigger_Dodging);
+                break;
+            case Moves.Uppercut:
+                _anim.SetTrigger(Anim_Trigger_Uppercut);
                 break;
             case Moves.Idle:
                 //reset everything
-                _anim.SetBool(Anim_WalkingBackward, false);
-                _anim.SetBool(Anim_WalkingForward, false);
+                _anim.SetBool(Anim_Bool_Walking_Backward, false);
+                _anim.SetBool(Anim_Bool_Walking_Forward, false);
                 break;
         }
-        _currentMove = move;
+
+        //cancel any double tap counter, unless we pressed jab
+        if (move != Moves.Jabbing && move != Moves.Idle)
+        {
+            doubleTapStarted = 0;
+            doubleTapCount = 0;
+        }
+
+    }
+
+    bool IsCurrentMove(Moves move)
+    {
+        switch (move)
+        {
+            case Moves.Idle:
+                return _anim.GetCurrentAnimatorStateInfo(0).IsName(Anim_Idle);
+            case Moves.Jabbing:
+                return _anim.GetCurrentAnimatorStateInfo(0).IsName(Anim_Jab);
+            case Moves.OneTwo:
+                return _anim.GetCurrentAnimatorStateInfo(0).IsName(Anim_One_Two);
+            case Moves.MovingForward:
+                return _anim.GetCurrentAnimatorStateInfo(0).IsName(Anim_Walk_Forward);
+            case Moves.MovingBackward:
+                return _anim.GetCurrentAnimatorStateInfo(0).IsName(Anim_Walk_Backward);
+            case Moves.Dodge:
+                return _anim.GetCurrentAnimatorStateInfo(0).IsName(Anim_Dodge);
+            case Moves.Uppercut:
+                return _anim.GetCurrentAnimatorStateInfo(0).IsName(Anim_Uppercut);
+            default:
+                return false;
+        }
     }
 
     void HandleInputs()
     {
         //move forward
-        if (_currentMove != Moves.MovingForward 
-            && _timeholdingAttackKey > 0 && _timeholdingAttackKey > HoldingKeyTime)
+        if (KeyHeldFor(_attackKey) > KeyLongPressTime)
         {
             DoMove(Moves.MovingForward);
         }
         //move backward
-        else if (_currentMove != Moves.MovingBackward && 
-            _timeholdingDodgeKey > 0 && _timeholdingDodgeKey > HoldingKeyTime)
+        else if (KeyHeldFor(_dodgeKey) > KeyLongPressTime)
         {
             DoMove(Moves.MovingBackward);
         }
-        else if (_currentMove != Moves.Idle && !Input.anyKey)
-        {
-            DoMove(Moves.Idle);
-        }
-
-    }
-
-    void ScanInputs()
-    {
-        //detect holding
-        if (Input.GetKey(_attackKey))
-        {
-            _timeholdingAttackKey += Time.deltaTime;
-        }
-        else
-        {
-            _timeholdingAttackKey = 0;
-        }
-
-
-        if (Input.GetKey(_dodgeKey))
-        {
-            _timeholdingDodgeKey += Time.deltaTime;
-        }
-        else
-        {
-            _timeholdingDodgeKey = 0;
-        }
         
+
     }
+
+    
 
 }
